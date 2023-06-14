2be5d7707ff2a12dde40922ca02a610560d534b9
kn5suzuki <suzukikengo@g.ecc.u-tokyo.ac.jp>
2023-06-14 17:38:01 +0900

    edit markdownview_type

--- api/v1/markdownview_types.go
+++ api/v1/markdownview_types.go
@@ -25,18 +25,32 @@ import (
 
 // MarkdownViewSpec defines the desired state of MarkdownView
 type MarkdownViewSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// Markdowns contain the markdown files you want to display.
+	// The key indicates the file name and must not overlap with the keys.
+	// The value is the content in markdown format.
+	//+kubebuilder:validation:Required
+	//+kubebuilder:validation:MinProperties=1
+	Markdowns map[string]string `json:"markdowns,omitempty"`
 
-	// Foo is an example field of MarkdownView. Edit markdownview_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// Replicas is the number of viewers.
+	//+kubebuilder:default=1
+	//+optional
+	Replicas int32 `json:"replicas,omitempty"`
+
+	// ViewerImage is the image name of the viewer.
+	//+optional
+	ViewerImage string `json:"viewerImage,omitempty"`
 }
 
 // MarkdownViewStatus defines the observed state of MarkdownView
-type MarkdownViewStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-}
+// +kubebuilder:validation:Enum=NotReady;Available;Healthy
+type MarkdownViewStatus string
+
+const (
+	MarkdownViewNotReady  = MarkdownViewStatus("NotReady")
+	MarkdownViewAvailable = MarkdownViewStatus("Available")
+	MarkdownViewHealthy   = MarkdownViewStatus("Healthy")
+)
 
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
