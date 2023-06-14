03b2873987b693192adb665511488b231e0e96dc
kn5suzuki <suzukikengo@g.ecc.u-tokyo.ac.jp>
2023-06-14 21:35:57 +0900

    created webhook and passed test

--- internal/controller/markdownview_controller.go
+++ internal/controller/markdownview_controller.go
@@ -51,9 +51,9 @@ type MarkdownViewReconciler struct {
 //! [reconciler]
 
 //! [rbac]
-//+kubebuilder:rbac:groups=view.zoetrope.github.io,resources=markdownviews,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=view.zoetrope.github.io,resources=markdownviews/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=view.zoetrope.github.io,resources=markdownviews/finalizers,verbs=update
+//+kubebuilder:rbac:groups=view.kn5suzuki.github.io,resources=markdownviews,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=view.kn5suzuki.github.io,resources=markdownviews/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=view.kn5suzuki.github.io,resources=markdownviews/finalizers,verbs=update
 //+kubebuilder:rbac:groups=core,resources=configmaps,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
